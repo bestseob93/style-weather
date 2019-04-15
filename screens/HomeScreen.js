9f84fe5195a52442402c9da869d0ed8e015d1a62
Hwan Seob Lee <bestseob93@gmail.com>
2019-04-16 00:24:11 +0900

    expo react-native init

---
+++ screens/HomeScreen.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { WebBrowser } from 'expo';
+
+import { MonoText } from '../components/StyledText';
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.contentContainer}>
+          <Text>hi</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+});
