c3aa99824e306b01f07324c22fe5d267072157ef
Hwan Seob Lee <bestseob93@gmail.com>
2019-04-16 23:10:03 +0900

    Back up App.js, Added Weather Mock

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,7 +9,7 @@ import {
   View,
 } from 'react-native';
 import { WebBrowser } from 'expo';
-
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { MonoText } from '../components/StyledText';
 
 export default class HomeScreen extends React.Component {
@@ -19,11 +19,16 @@ export default class HomeScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.contentContainer}>
-          <Text>hi</Text>
+      <ScrollView style={styles.container}>
+        <View style={styles.headerContainer}>
+          <MaterialCommunityIcons size={48} name="weather-sunny" color={'#fff'} />
+          <Text style={styles.tempText}>Temperature˚</Text>
         </View>
-      </View>
+        <View style={styles.contentContainer}>
+          <Text style={styles.title}>So Sunny</Text>
+          <Text style={styles.subtitle}>It hurts my eyes!</Text>
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -31,9 +36,30 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f7b733',
+  },
+  headerContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  tempText: {
+    fontSize: 48,
+    color: '#fff'
   },
   contentContainer: {
-    paddingTop: 30,
+    flex: 2,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-end',
+    paddingLeft: 25,
+    marginBottom: 40
   },
+  title: {
+    fontSize: 48,
+    color: '#fff'
+  },
+  subtitle: {
+    fontSize: 24,
+    color: '#fff'
+  }
 });
